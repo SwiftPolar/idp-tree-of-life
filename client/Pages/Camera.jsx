5f019debc37e792852352b980699fcc4beaad7b7
Lee Qixian <qixian@leeqixian.com>
2016-02-21 23:39:45 +0800

    Testing some mobile platform stuff on android/ios. Modified camera function to test camera out.

--- client/Pages/Camera.jsx
+++ client/Pages/Camera.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import { browserHistory } from 'react-router';
 
 export default class extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            image: null
+        }
         MeteorCamera.getPicture((error, data) => {
-           if(error) {
-               console.log("CAMERA ERROR!");
-           } else {
-               console.log(data);
-           }
+            if (error) {
+                browserHistory.goBack();
+            } else {
+                this.setState({image: data});
+            }
         });
     }
+
     render() {
         return (
-            <div>
-                CAMERA
+            <div className="ui grid">
+                <div className="row">
+                    <div className="fifteen wide column centered">
+                        <img class="ui fluid image" src={this.state.image}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="fifteen wide column row centered">
+                        <div className="ui form" id="form">
+                            <div className="field">
+                                <label>Description</label>
+                                <textarea></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+
+                    </div>
+                </div>
             </div>
         )
     }
