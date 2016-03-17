7ef016132cde7d3785a764cf7370f3c616029205
Lee Qixian <qixian@leeqixian.com>
2016-03-17 18:20:17 +0800

    Formatting of code.

--- client/Forums/ViewTopicHeader.jsx
+++ client/Forums/ViewTopicHeader.jsx
@@ -58,36 +58,37 @@ export default class extends React.Component {
     render() {
         return (
             <Sticky>
-            <Toolbar styles={{position: 'fixed'}}>
-                <ToolbarGroup float="left" firstChild={true}>
-                    <IconButton onTouchTap={()=>{browserHistory.goBack()}}><BackIcon /></IconButton>
-                </ToolbarGroup>
-                <ToolbarGroup float="left">
+                <Toolbar styles={{position: 'fixed'}}>
+                    <ToolbarGroup float="left" firstChild={true}>
+                        <IconButton onTouchTap={()=>{browserHistory.goBack()}}><BackIcon /></IconButton>
+                    </ToolbarGroup>
+                    <ToolbarGroup float="left">
 
-                    <RaisedButton label="Reply" primary={true} onTouchTap={this.newReply.bind(this)}/>
+                        <RaisedButton label="Reply" primary={true} onTouchTap={this.newReply.bind(this)}/>
 
-                </ToolbarGroup>
-                <ToolbarGroup float="right">
-                    <IconButton onTouchTap={this.openSearch.bind(this)}><SearchIcon onTouchTap={this.openSearch.bind(this)} /></IconButton>
-                    <Popover open={this.state.searchOpen}
-                             anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-                             targetOrigin={{horizontal: 'left', vertical: 'top'}}
-                             anchorEl={this.state.searchAnchor}
-                             onRequestClose={this.closeSearch.bind(this)}>
-                        <TextField
-                            style={{paddingLeft: '5px', paddingRight: '5px'}}
-                            hintText="Enter Topic"
-                            floatingLabelText="Search"
-                            fullWidth={true}
-                            value={this.state.searchValue}
-                            onChange={this.handleSearch.bind(this)}
-                            onEnterKeyDown={this.submitSearch.bind(this)}
-                        />
-                        <RaisedButton label="Search" onTouchTap={this.submitSearch.bind(this)} />
-                    </Popover>
-                </ToolbarGroup>
-            </Toolbar>
-                </Sticky>
+                    </ToolbarGroup>
+                    <ToolbarGroup float="right">
+                        <IconButton onTouchTap={this.openSearch.bind(this)}><SearchIcon
+                            onTouchTap={this.openSearch.bind(this)}/></IconButton>
+                        <Popover open={this.state.searchOpen}
+                                 anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+                                 targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                                 anchorEl={this.state.searchAnchor}
+                                 onRequestClose={this.closeSearch.bind(this)}>
+                            <TextField
+                                style={{paddingLeft: '5px', paddingRight: '5px'}}
+                                hintText="Enter Topic"
+                                floatingLabelText="Search"
+                                fullWidth={true}
+                                value={this.state.searchValue}
+                                onChange={this.handleSearch.bind(this)}
+                                onEnterKeyDown={this.submitSearch.bind(this)}
+                            />
+                            <RaisedButton label="Search" onTouchTap={this.submitSearch.bind(this)}/>
+                        </Popover>
+                    </ToolbarGroup>
+                </Toolbar>
+            </Sticky>
         );
     }
 }
